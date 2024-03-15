b87e68f759dbd1c585c402a767a48d34f98f41a9
Tomi Koskinen <tomi@topeko.dev>
2024-03-15 17:14:07 +0200

    Global css files to new folder - import styles from script - delete empty js file in lib

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script>
+    // Styles
+    import '$lib/styles/app.css';
+    import '$lib/styles/blog-content.css';
     // Components
     import Navbar from '$lib/components/Nav.svelte';
     import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <svelte:head>
-    <link rel="stylesheet" href="/styles/app.css">
-    <link rel="stylesheet" href="/styles/blog-content.css">
+    
 </svelte:head>
 
 <Navbar />
